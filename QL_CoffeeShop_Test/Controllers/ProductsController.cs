a355ec2e4e4b41e37ceafb665f75fa12b4473fc5
LAPTOP <LAPTOP@LAPTOP-EKGEPIQH>
2026-05-08 08:20:31 +0700

    Finish Season 1Add project files.

---
+++ QL_CoffeeShop_Test/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using QL_CoffeeShop_Test.Models.interfaces;
+using Microsoft.AspNetCore.Mvc;
+namespace QL_CoffeeShop_Test.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
---
+++ QL_CoffeeShop_Test/Models/Service/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using QL_CoffeeShop_Test.Models.interfaces;
+namespace QL_CoffeeShop_Test.Models.Service
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> ProductsList = new List<Product>()
+        {
+            new Product{Id = 1, Name = "America", Price = 25, detail = "Name product", ImageUrl ="http://index.com" },
+            new Product{Id = 2, Name = "Latte", Price = 30, detail = "Name product", ImageUrl ="http://index.com" },
+            new Product{Id = 3, Name = "Cappuccino", Price = 35, detail = "Name product", ImageUrl ="http://index.com" },
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return ProductsList;
+        }
+
+
+        public Product GetProductDetail(int id)
+        {
+            return ProductsList.FirstOrDefault(p => p.Id == id);
+        }
+
+        
+
+        public IEnumerable<Product> GetTrendingProducts()
+        {
+            return ProductsList.Where(p => p.IsTrendingProduct);
+        }
+    }
+}
